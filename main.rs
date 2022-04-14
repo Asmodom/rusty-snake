4c5ebbf03d8f5d48bc4b03dd919a502d4b57be8a
Asmodom <donny.nijssen@gmail.com>
2022-04-14 23:18:01 +0200

    initial project setup for rusty snake

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let mut message ="Hello, World";
+    println!("Some text: {}", message);
+    message = "Hi there";
+    println!("Some text: {}", message);
+
+
+}
+
+//rustc main.rs
